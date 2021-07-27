923b4c67c0f120a3646d725622f9f95ef17f86d6
VictoriaMR <849376723@qq.com>
2021-07-27 23:59:13 +0800

    debug

--- app/controller/bag/IndexController.php
+++ app/controller/bag/IndexController.php
@@ -9,11 +9,20 @@ class IndexController extends Controller
 	{	
 		html()->addCss();
 		html()->addJs();
+		html()->addJs('slider');
 
 		//分类列表
 		$hotCategory = make('app/service/CategoryService')->getHotCategory(16);
+		$banner = [];
+		for ($i=1;$i<6;$i++) {
+			$banner[] = [
+				'title' => '',
+				'image' => siteUrl('image/mobile/banner/banner'.$i.'.jpg'),
+				'url'=> '',
+			];
+		}
+		$this->assign('banner', $banner);
 		$this->assign('hot_category', array_chunk($hotCategory, 2));
-
 		$this->view();
 	}
 }
--- bag/view/mobile/index/index.php
+++ bag/view/mobile/index/index.php
@@ -1,17 +1,40 @@
-<?php if (!empty($hot_category)){?>
-<div class="bg-round"></div>
-<ul class="hot-category-list">
-	<?php foreach ($hot_category as $value){?>
-	<li>
-		<?php foreach ($value as $item){?>
-		<a href="<?php echo $item['url'];?>" class="block f12">
-			<div class="cate-img">
-				<img data-src="<?php echo $item['avatar'];?>" src="<?php echo siteUrl('image/common/noimg.svg');?>" class="lazyload">
-			</div>
-			<p class="e1"><?php echo $item['name'];?></p>
-		</a>
+<?php if (!empty($banner)) { ?>
+<div class="banner relative">
+	<ul>
+		<?php foreach ($banner as $key => $value){?>
+		<li class="sw-slides" data-index="<?php echo $key;?>">
+			<a href="<?php echo $value['url'];?>">
+				<img src="<?php echo $value['image'];?>">
+			</a>
+		</li>
 		<?php } ?>
-	</li>
-	<?php } ?>
-</ul>
+	</ul>
+	<ol class="pop-content">
+		<?php foreach ($banner as $key => $value){?>
+		<li <?php echo $key==0?'class="active"':'';?> data-index="<?php echo $key;?>"></li>
+		<?php } ?>
+	</ol>
+</div>
 <?php } ?>
+<?php if (!empty($hot_category)){?>
+<div class="category-content">
+	<div class="bg-round"></div>
+	<ul class="hot-category-list">
+		<?php foreach ($hot_category as $value){?>
+		<li>
+			<?php foreach ($value as $item){?>
+			<a href="<?php echo $item['url'];?>" class="block f12">
+				<div class="cate-img">
+					<img data-src="<?php echo $item['avatar'];?>" src="<?php echo siteUrl('image/common/noimg.svg');?>" class="lazyload">
+				</div>
+				<p class="e1"><?php echo $item['name'];?></p>
+			</a>
+			<?php } ?>
+		</li>
+		<?php } ?>
+	</ul>
+</div>
+<?php } ?>
+<script type="text/javascript">
+swipeslider($('.banner'));
+</script>
