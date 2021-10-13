ab112f49de42decc680fe3c265d9801be6845321
LiaoMingRong <849376723@qq.com>
2021-10-13 16:57:47 +0800

    date.timezone

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 define('APP_MEMORY_START', memory_get_usage());
+ini_set('date.timezone', 'Asia/Shanghai');
 define('APP_TIME_START', microtime(true));
 define('DS', '/');
 define('ROOT_PATH', strtr(__DIR__, '\\', '/').DS);
