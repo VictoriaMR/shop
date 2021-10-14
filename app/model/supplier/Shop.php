8aacfe06edc2830f76294d149e75f75a46394873
LiaoMingRong <849376723@qq.com>
2021-10-14 14:51:03 +0800

    debug

--- app/model/supplier/Shop.php
+++ app/model/supplier/Shop.php
@@ -6,7 +6,7 @@ use app\model\Base;
 class Shop extends Base
 {
 	protected $_table = 'supplier_shop';
-	protected $_addTime = 'shop_id';
+	protected $_primaryKey = 'shop_id';
 	protected $_addTime = 'add_time';
 	protected $_updateTime = 'update_time';
 }
--- app/model/supplier/Url.php
+++ app/model/supplier/Url.php
@@ -6,5 +6,6 @@ use app\model\Base;
 class Url extends Base
 {
 	protected $_table = 'supplier_url';
+	protected $_primaryKey = 'supp_id';
 	protected $_addTime = 'add_time';
 }
