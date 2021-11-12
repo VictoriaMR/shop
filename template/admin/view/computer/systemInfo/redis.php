c433ee2687428f2bb829118a03960c5f9a76af2a
LiaoMingRong <849376723@qq.com>
2021-11-12 16:12:47 +0800

    debug

--- template/admin/view/computer/systemInfo/redis.php
+++ template/admin/view/computer/systemInfo/redis.php
@@ -4,10 +4,16 @@
             <th class="col1">名称</th>
             <th class="col6">值</th>
         </tr>
+        <?php if (!empty($info) && is_array($info)){?>
         <?php foreach ($info as $key=>$val){?>
         <tr>
             <td class="col1"><?php echo $key;?></td>
             <td class="col6"><?php echo $val;?></td>
+        </tr>
+        <?php }?>
+        <?php } else {?>
+        <tr>
+            <td colspan="2" class="orange tc">Redis链接不正常</td>
         </tr>
         <?php }?>
     </table>
