f7132c53b5b26eac86ecb305cdb6d405b3e9a382
LiaoMingRong <849376723@qq.com>
2021-11-17 09:18:51 +0800

    debue

--- app/controller/admin/Task.php
+++ app/controller/admin/Task.php
@@ -34,16 +34,16 @@ class Task extends AdminBase
 
 	protected function taskList()
 	{
-		$taskList = redis(2)->smembers(self::TASKPREFIX.'all');
+		$taskList = cache(2)->smembers(self::TASKPREFIX.'all');
 		$list = [];
 		if (!empty($taskList)) {
 			$mainIndex = array_search('app-task-MainTask', $taskList);
-			$data = redis(2)->hGetAll(self::TASKPREFIX.$taskList[$mainIndex]);
+			$data = cache(2)->hGetAll(self::TASKPREFIX.$taskList[$mainIndex]);
 			$data['name'] = $taskList[$mainIndex];
 			$list[] = $data;
 			unset($taskList[$mainIndex]);
 			foreach($taskList as $value) {
-				$data = redis(2)->hGetAll(self::TASKPREFIX.$value);
+				$data = cache(2)->hGetAll(self::TASKPREFIX.$value);
 				$data['name'] = $value;
 				$list[] = $data;
 			}
@@ -57,10 +57,10 @@ class Task extends AdminBase
 		$key = ipost('key');
 		$enabled = config('task.enabled');
 		if (in_array($type, ['startup_all', 'shutdown_all'])) {
-			$taskList = redis(2)->smembers(self::TASKPREFIX.'all');
+			$taskList = cache(2)->smembers(self::TASKPREFIX.'all');
 			if (!empty($taskList)) {
 				foreach($taskList as $value) {
-					redis(2)->hSet(self::TASKPREFIX.$value, 'boot', $type == 'startup_all' && $enabled ? 'on' : 'off');
+					cache(2)->hSet(self::TASKPREFIX.$value, 'boot', $type == 'startup_all' && $enabled ? 'on' : 'off');
 				}
 			}
 			if ($type == 'startup_all' && $enabled) {
@@ -71,9 +71,9 @@ class Task extends AdminBase
 				$this->error('进程名称不能为空');
 			}
 			if ($type == 'shutdown') {
-				redis(2)->del(self::LOCKERPREFIX.$key);
+				cache(2)->del(self::LOCKERPREFIX.$key);
 			}
-			redis(2)->hSet(self::TASKPREFIX.$key, 'boot', $type == 'startup' && $enabled ? 'on' : 'off');
+			cache(2)->hSet(self::TASKPREFIX.$key, 'boot', $type == 'startup' && $enabled ? 'on' : 'off');
 			if (!$enabled) {
 				$this->error('当前任务配置未开启');
 			}
