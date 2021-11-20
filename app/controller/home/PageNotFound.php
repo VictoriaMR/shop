3d1f56a84bd7e3a65b2bd705274817cedabb098d
VictoriaMR <849376723@qq.com>
2021-11-21 01:41:25 +0800

    dbeug

--- app/controller/home/PageNotFound.php
+++ app/controller/home/PageNotFound.php
@@ -7,12 +7,12 @@ class PageNotFound extends HomeBase
 {
 	public function index()
 	{	
-		html()->addCss();
-		html()->addCss('common/productList');
+		// html()->addCss();
+		// html()->addCss('common/productList');
 
-		$siteName = site()->getName();
-		$this->assign('siteName', $siteName);
-		$this->assign('_title', 'Sorry, Page not found - '.$siteName);
+		// $siteName = site()->getName();
+		// $this->assign('siteName', $siteName);
+		// $this->assign('_title', 'Sorry, Page not found - '.$siteName);
 		$this->view();
 	}
 }
