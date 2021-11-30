ad87116ab66bafa696584ee58ac69f9ad61cb304
LiaoMingRong <849376723@qq.com>
2021-11-30 13:38:09 +0800

    debug

--- app/service/Widget.php
+++ app/service/Widget.php
@@ -12,14 +12,20 @@ class Widget
 		$diff = $page - $middleNum;
 		//向上翻页箭头
 		$pageBarHtml .= '<li class="arrow-li" data-page="'.($page - 1).'"><a href="'.''.'"><span class="iconfont icon-xiangzuo1"></span></a></li>';
-		if ($diff > 0) {
-			for ($i=$diff; $i < $page; $i++) { 
+		//左侧页码
+		if ($page > 1) {
+			for ($i=($diff>0?$diff:1); $i<$page; $i++) { 
 				$pageBarHtml .= '<li class="arrow-li" data-page="'.$i.'"><a href="'.''.'">'.$i.'</a></li>';
 			}
 		}
 		$pageBarHtml .= '<li class="arrow-li" data-page="'.$page.'"><a href="'.''.'">'.$page.'</a></li>';
 		if ($page < $totalPage) {
-			for ($i=$page+1; $i < $buttonNumber-$diff; $i++) { 
+			$end = $page+$middleNum;
+			if ($diff < 0) {
+				$end = $end - $diff;
+			}
+			$end = $end>$totalPage?$totalPage:$end;
+			for ($i=$page+1; $i<=$end; $i++) { 
 				$pageBarHtml .= '<li class="arrow-li" data-page="'.$i.'"><a href="'.''.'">'.$i.'</a></li>';
 			}
 		}
