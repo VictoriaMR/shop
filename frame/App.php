f49930a8dc3731010a9f021f9b256235504b9550
LiaoMingRong <849376723@qq.com>
2022-02-23 16:13:16 +0800

    debug

--- frame/App.php
+++ frame/App.php
@@ -25,7 +25,7 @@ class App
 		$router['view_suffix'] = $baseInfo['view_suffix'];
 		define('APP_TEMPLATE_TYPE', $baseInfo['type']);
 		define('APP_TEMPLATE_PATH', $baseInfo['path']);
-		define('APP_DOMAIN', 'https://'.$_SERVER['HTTP_HOST'].'/');
+		define('APP_DOMAIN', $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].'/');
 		self::set('router', $router);
 		//执行方法
 		$controller = 'app/controller/'.$router['class'].'/'.$router['path'];
