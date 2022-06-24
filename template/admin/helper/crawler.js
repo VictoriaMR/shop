2e85fd3f0364912d7116028ad8edfadd87fe711f
LiaoMingRong <849376723@qq.com>
2022-06-24 17:47:14 +0800

    debug

--- template/admin/helper/crawler.js
+++ template/admin/helper/crawler.js
@@ -87,9 +87,9 @@ var CRAWLER = {
         return ret;
     },
     isDescPic: function(src) {
-        var ignore = ['img.taobao.com', 'ma.m.1688.com', 'amos.alicdn.com', 'alisoft.com', 'add_to_favorites.htm', 'img.alicdn.com/NewGualianyingxiao'];
+        var ignore = ['img.taobao.com', 'ma.m.1688.com', 'amos.alicdn.com', 'alisoft.com', 'add_to_favorites.htm', 'img.alicdn.com/NewGualianyingxiao', '_.webp'];
         for(var i=0; i<ignore.length; i++){
-            if(src.indexOf(ignore[i])!=-1 || src.indexOf('_.webp')===-1){
+            if(src.indexOf(ignore[i])!=-1){
                 return false;
             }
         }
