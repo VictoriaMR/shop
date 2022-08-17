a0a52c8d225ffb5fe759c1811e68c1723e08309e
LiaoMingRong <849376723@qq.com>
2022-08-17 19:06:31 +0800

    资源优化

--- template/newsme/view/computer/common/header.php
+++ template/newsme/view/computer/common/header.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 	<title><?php echo $_title??appT('_title');?></title>
-	<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
-	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no"/>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-	<meta http-equiv="cache-control" content="public"/>
+	<meta http-equiv="cache-control" content="no-cache"/>
 	<meta content="telephone=no,email=no" name="format-detection"/>
 	<meta name="description" content="<?php echo $_desc??appT('_desc');?>">
 	<meta name="keywords" content="<?php echo $_keyword??appT('_keyword');?>">
