6087959fe35561640712e4f8c92fa65f8d9e624f
VictoriaMR <849376723@qq.com>
2022-08-20 21:32:36 +0800

    模板缓存

--- frame/View.php
+++ frame/View.php
@@ -13,7 +13,7 @@ class View
 
 	protected function fetch($template, array $data=[], $cache=false)
 	{	
-		if ($cache && \App::get('base_info', 'cache') && !config('env', 'APP_DEBUG')) {
+		if ($cache && \App::get('base_info', 'cache')) {
 			$request_uri = trim($_SERVER['REQUEST_URI'], '/');
 			if (empty(explode('.html', $request_uri)[1])) {
 				$content = $this->getContent($template, $data);
