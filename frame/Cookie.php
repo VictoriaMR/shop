7da401cdfa60a5b686e5f1898085b1ec0e85f59f
LiaoMingRong <849376723@qq.com>
2022-09-16 09:50:49 +0800

    cookie 设置

--- frame/Cookie.php
+++ frame/Cookie.php
@@ -9,7 +9,7 @@ class Cookie
 		'path' => '/',
 		'domain' => '',
 		'secure' => true,
-		'httponly' => true,
+		'httponly' => false,
 	];
 
 	public function init()
@@ -74,7 +74,6 @@ class Cookie
 		$expire = empty($config['expire']) ? 0 : $_SERVER['REQUEST_TIME'] + intval($config['expire']);
 		$_COOKIE[$name] = $value;
 		session()->set(APP_TEMPLATE_TYPE.'_info', $value, $name);
-    	header('Access-Control-Allow-Credentials:true');
 		return setcookie($name, $value, $expire, $config['path'], $config['domain'], $config['secure'], $config['httponly']);
 	}
 
