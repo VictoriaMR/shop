fdb8fee121293c2acb291db54096a2298710bf10
LiaoMingRong <849376723@qq.com>
2023-09-21 17:27:02 +0800

    全局样式调整

--- template/clothes/js/mobile/common.js
+++ template/clothes/js/mobile/common.js
@@ -18,3 +18,27 @@ function start(obj) {
     }
     obj.style.cssText += 'overflow-y:auto';
 }
+// 头部部件滚动监听
+window.onload = function(){
+    var headerObj = document.getElementById('header');
+    var headerH = headerObj.offsetHeight;
+    var nowH = 0;
+    var up = false;
+    var down = false;
+    window.addEventListener('scroll', function() {
+        var scroH = document.documentElement.scrollTop;
+        console.log(scroH, nowH)
+        if (scroH > nowH) {
+            if (scroH > 50 && !up) {
+                headerObj.style.top = '-'+headerH+'px';
+                up = true;
+                down = false;
+            }
+        } else if(!down) {
+            headerObj.style.top = 0;
+            down = true;
+            up = false;
+        }
+        nowH = scroH;
+    });
+};
